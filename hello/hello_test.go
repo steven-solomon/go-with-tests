3d087601e0db27bb408bf543fc0e8d5d850b5909
Steven Solomon <steven-solomon@github.com>
2023-11-22 17:00:01 +0000

    Add default message

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -11,4 +11,13 @@ func TestHello(t *testing.T) {
 			t.Errorf("got %q want %q", got, want)
 		}
 	})
+
+	t.Run("sayin 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
 }
